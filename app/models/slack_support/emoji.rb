57698f1c793ab6acd46c2d394d5c1af862b28ab9
Koki Oyatsu <kouki@weseek.co.jp>
2018-09-20 12:43:54 +0900

    bugfix for emoji list nil check.

--- app/models/slack_support/emoji.rb
+++ app/models/slack_support/emoji.rb
@@ -13,7 +13,8 @@ module SlackSupport
     def list
       @list ||= Rails.cache.fetch('slack_support__emoji#list', expires_in: 30.minutes) do
         @client.emoji_list['emoji'].tap do |list|
-          list.map do |key, value|
+          # nil check
+          (list || []).map do |key, value|
             if value =~ /\Aalias:(.+)\z/
               list[key] = list[$1]
             end
